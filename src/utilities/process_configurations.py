1bbc43f5ea2d93a644d7633cedb2f785777e048e
bryce-seefieldt <bryce.seefieldt@gmail.com>
2023-11-03 15:41:55 -0400

    Contribution Install Tools: * Installed Black Formatting, Flake8 Linter * Added CONTRIBUTING.md and pyproject.toml

--- src/utilities/process_configurations.py
+++ src/utilities/process_configurations.py
@@ -1,11 +1,14 @@
 import tomllib
 
+
 # Load arguments from a .toml configuration file
 def loadConfig(configPath):
     try:
         with open(configPath, "rb") as f:
             config = tomllib.load(f)
     except FileNotFoundError as e:
-        raise Exception(f"Configuration file '{configPath}' could not be found.")
+        raise Exception(
+            f"{e}:Configuration file '{configPath}' could not be found."
+        )
 
     return config
