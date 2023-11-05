37d25c72f404c43c0bef1528901be6f621313141
bryce-seefieldt <bryce.seefieldt@gmail.com>
2023-11-05 16:32:00 -0500

    "Final Flake8 and Black"

--- src/utilities/process_configurations.py
+++ src/utilities/process_configurations.py
@@ -7,6 +7,8 @@ def loadConfig(configPath):
         with open(configPath, "rb") as f:
             config = tomllib.load(f)
     except FileNotFoundError as e:
-        raise Exception(f"{e}:Configuration file '{configPath}' could not be found.")
+        raise Exception(
+            f"{e}:Configuration file '{configPath}' could not be found."
+        )
 
     return config
